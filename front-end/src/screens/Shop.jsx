0665e19543c40edb3956528ba5da7a6b2d012d07
stango1234556 <stango1234556@gmail.com>
2026-03-23 01:24:05 -0400

    shop changes and linking buttons

--- front-end/src/screens/Shop.jsx
+++ front-end/src/screens/Shop.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Shop.css";
 import { PlayerContext } from "../context/Player_Context";
 import Item_Card from "../components/Item_Card";
 
-function Shop({ goHome }) {
+function Shop() {
   const { money, inventory, buyItem } = useContext(PlayerContext);
+  const navigate = useNavigate();
 
   const items = [
     { id: 1, name: "House Upgrade", info: "Improves housing", price: 200 },
@@ -16,27 +18,23 @@ function Shop({ goHome }) {
   return (
     <div className="shop-page">
       <div className="shop-screen">
-
-        {/* TOP BAR */}
         <div className="shop-top">
-          <div className="player-icon">Player Icon</div>
+          <div className="shop-player-icon">Player Icon</div>
 
-          <div className="user-text">
-            <div className="username">Username</div>
-            <div className="userid">${money}</div>
+          <div className="shop-user-text">
+            <div className="shop-username">Username</div>
+            <div className="shop-userid">${money}</div>
           </div>
 
-          <button className="close-btn" onClick={() => navigate("/city-layout")}>
+          <button className="shop-close-btn" onClick={() => navigate("/city-layout")}>
             ×
           </button>
         </div>
 
-        {/* HEADER ROW */}
         <div className="shop-header-row">
           <div className="shop-title">Shop</div>
         </div>
 
-        {/* LIST */}
         <div className="shop-list">
           {items.map((item) => (
             <Item_Card
@@ -49,11 +47,9 @@ function Shop({ goHome }) {
           ))}
         </div>
 
-        {/* BOTTOM */}
         <div className="shop-bottom">
           <div>Build your city</div>
         </div>
-
       </div>
     </div>
   );
