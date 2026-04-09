5921bc408027458d28ea18813b706136d808d9ca
ml8397 <ml8397@nyu.edu>
2026-04-09 01:40:58 -0400

    goal structure set

---
+++ back-end/data/budgetGoalsStore.js
@@ -0,0 +1,23 @@
+let budgetGoals = {
+    alex: {
+        total: { goal: 10000 , current: 5000 },
+        food: { goal: 2000, current: 1000 },
+        housing: { goal: 4000, current: 2000 },
+        health: { goal: 3000, current: 1500 },
+        Entertainment: { goal: 1000, current: 500 },
+    },
+    jordy88: {
+        total: { goal: 5000, current: 2500 },
+        food: { goal: 1000, current: 500 },
+        housing: { goal: 2000, current: 1000 },
+        health: { goal: 1000, current: 500 },
+        Entertainment: { goal: 1000, current: 500 },
+    },
+    caseybuilds: {
+        total: { goal: 7000, current: 3500 },
+        food: { goal: 2000, current: 1000 },
+        housing: { goal: 3000, current: 1500 },
+        health: { goal: 500, current: 250 },
+        Entertainment: { goal: 1500, current: 750 },
+    },
+};
