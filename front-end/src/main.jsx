b52e8b54a4d4efb5c1830257f1557f8e69e22e6c
ml8397 <ml8397@nyu.edu>
2026-04-13 23:02:02 -0400

    citylayout bug fixed

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/Auth_Context.jsx";
+import { BuildingProvider } from "./context/Building_Context.jsx";
 import { FriendsProvider } from "./context/Friends_Context.jsx";
 import { PlayerProvider } from "./context/Player_Context.jsx";
 import "./index.css";
@@ -10,9 +11,11 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
       <PlayerProvider>
+        <BuildingProvider>
           <FriendsProvider>
             <App />
           </FriendsProvider>
+        </BuildingProvider>
       </PlayerProvider>
     </AuthProvider>
   </StrictMode>
